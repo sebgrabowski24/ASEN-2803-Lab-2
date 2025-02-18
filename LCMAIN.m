8314f4bf721b44a53698c781fe8164f4a93a4f85
LucasNussbaumer <lunu2337@colorado.edu>
2025-02-18 12:11:02 -0700

    Main Script
    Added main script and the data/assignment files

---
+++ LCMAIN.m
@@ -0,0 +1,20 @@
+%{
+Authors: Lucas Nussbaumer, Chase Wyngarden
+Assignment: Lab 2
+Creation Date: Febuary 18, 2025
+Inputs: Test1_ (10.5 volt - 5.5 volt)
+Outputs: 
+Purpose: Analyze general planar motion, Practice using kinematical 
+descriptions of a physical system Investigate discrepancies between a model 
+and a physical systes, Assess experimental results
+%}
+
+
+%% Housekeeping
+
+clc
+clear
+close all
+
+
+
