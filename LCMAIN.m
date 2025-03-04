c6f57c4b432e82783c55df0447905d4355ce6590
LucasNussbaumer <lunu2337@colorado.edu>
2025-03-04 10:43:59 -0700

    Update LCMAIN.m

--- LCMAIN.m
+++ LCMAIN.m
@@ -24,12 +24,13 @@ close all
 [~, ~, ~, ~, Data_8pt5V] = LCSDATA(readtable("Data Files/Test1_8pt5V"));
 [~, ~, ~, ~, Data_7pt5V] = LCSDATA(readtable("Data Files/Test1_7pt5V"));
 [~, ~, ~, ~, Data_6pt5V] = LCSDATA(readtable("Data Files/Test1_6pt5V"));
+[~, ~, ~, ~, Data_5pt5V] = LCSDATA(readtable("Data Files/Test1_5pt5V"));
 
-Datas = [Data_6pt5V, Data_7pt5V, Data_8pt5V, Data_9pt5V, Data_10pt5V];
+Datas = [Data_5pt5V, Data_6pt5V, Data_7pt5V, Data_8pt5V, Data_9pt5V, Data_10pt5V];
 %% Plot Exp Data
 
-for i = 1:5
-    LineName = 5.5 + i; % Figure name
+for i = 1:6
+    LineName = 4.5 + i; % Figure name
     
     figure(i)
     hold on
@@ -48,13 +49,13 @@ end
 
 %% Calculate and plot our model's velocity vs theta
 
-for i = 1:5
+for i = 1:6
     omega = mean(Datas(i).w_exp);
     theta =  Datas(i).theta_exp;
     model_v{i} = LCSMODEL(7.5, 26, 15.5, theta, omega);
 
 
-    LineName = 5.5 + i; % Figure name
+    LineName = 4.5 + i; % Figure name
     figure(i) % Specify figure to add to
     hold on
 
@@ -68,6 +69,11 @@ for i = 1:5
     hold off
 
 end
+
+theta=linspace(0,2160,1000);
+dat = LCSMODEL(7.5, 26, 15.5, theta, 1000);
+
+
 
 
 
